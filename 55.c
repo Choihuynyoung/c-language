e01de747baacb92eb176882dab5a128cef425960
최현영 <choeh3189@gmail.com>
2023-04-11 06:52:55 +0000

    55

--- 55.c
+++ 55.c
@@ -3,8 +3,14 @@ int main() {
 	
 	int a = 3;
 	a = a + 6;
-	if(a > 5) printf("%d\n",a);
-	//printf("%d\n", a - 6);
+	if(a < 5) {
+	printf("%d\n",a);
+	printf("조건이 참일 때, 실행됩니다. \n");
+	}else {
+	printf("%d\n",a);
+	printf("조건이 거짓일 때, 실행됩니다. \n");
+	}
+		//printf("%d\n", a - 6);
 	
 	return 0;
 	
